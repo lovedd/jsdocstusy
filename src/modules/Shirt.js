caacd51b99c6cde4e282ea363076b6b9820e9590
liuliu <liuliu@xiaoliuliudexiao.local>
2018-11-21 20:41:31 +0800

    first commit

---
+++ src/modules/Shirt.js
@@ -0,0 +1,15 @@
+// common module 模块
+/**
+ * Shirt module.
+ * @module modules/shirt
+ */
+
+/** Button the shirt. */
+exports.button = function() {
+    // ...
+};
+
+/** Unbutton the shirt. */
+exports.unbutton = function() {
+    // ...
+};
